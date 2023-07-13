3a9500ea5c98d1daa48b235ec84e50d3b26aedc2
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2023-07-13 23:16:19 +0100

    add lint scripts

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,28 +8,28 @@ interface ICard {
     style?: string
 }
 
-export default function Card({item, style}:ICard): JSX.Element{
+export default function Card(props:ICard): JSX.Element{
     const store = useCartStore()
 
     function addItem(){
-        const alreadyExist = store?.items.find((p) => p.id === item.id)
+        const alreadyExist = store?.items.find((p) => p.id === props.item.id)
 
         if(alreadyExist){
-            store?.setItems((p) => p.id === item.id, 'quantity',( cty) => cty ? cty + 1: 1)
+            store?.setItems((p) => p.id === props.item.id, 'quantity',( cty) => cty ? cty + 1: 1)
             return
         }
         
-        store?.setItems([...store.items, item])
+        store?.setItems([...store.items, props.item])
     }
    
     return(
-    <div class={`bg-white rounded-sm shadow-ms w-80 ${style}`}>
-        <img  class="h-40 w-full object-cover" src={item.image} alt={item?.name}/>
+    <div class={`bg-white rounded-sm shadow-ms w-80 ${props.style}`}>
+        <img  class="h-40 w-full object-cover" src={props.item.image} alt={props.item?.name}/>
         <div class="bg-white p-4 text-left rounded-sm shadow-ms">
-        <h2 class="text-slate-700 dark:text-slate-500">{item?.name}</h2>
+        <h2 class="text-slate-700 dark:text-slate-500">{props.item?.name}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quas ab laborum quia nisi temporibus.</p>
         <div class="grid gap-2 grid-cols-2 grid-rows-2 mt-2">
-             <h2 class="text-slate-700 dark:text-slate-500">{formatCurrency({amount:item?.price})}</h2>
+             <h2 class="text-slate-700 dark:text-slate-500">{formatCurrency({amount:props.item?.price})}</h2>
            <div class='text-primary cursor-pointer' onClick={() => addItem()}>
             Add to Cart
             </div> 
--- src/pages/ItemsList.tsx
+++ src/pages/ItemsList.tsx
@@ -1,21 +1,15 @@
-import {  For, JSX } from 'solid-js';
-import Card from '../components/Card';
-import data from '../context/data/cartData';
+import { For, JSX } from "solid-js";
+import Card from "../components/Card";
+import data from "../context/data/cartData";
 
 function ItemsList(): JSX.Element {
-
   return (
-   <div class="container m-auto">
-    <div class="grid grid-cols-4 gap-5 my-4">
-    <For each={data}>{
-      (item, i) => <Card item={item}/>
-    }</For>
+    <div class="container m-auto">
+      <div class="grid grid-cols-4 gap-5 my-4">
+        <For each={data}>{(item) => <Card item={item} />}</For>
+      </div>
     </div>
-   </div>
   );
 }
 
 export default ItemsList;
-
-
-
