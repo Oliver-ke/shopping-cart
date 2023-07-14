ce3268178f89014f214c6f143436c99b5d4c144e
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2023-07-14 21:24:10 +0100

    s3 sync

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync ./build-artifact $S3_BUCKET --acl public-read
+aws s3 sync ./build-artifact s3://$S3_BUCKET --acl public-read
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /*
