83c0e4e481d4d22fdbe29a3c26520a1a3f613b7b
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2023-07-15 13:24:03 +0100

    refactor to main

--- deployment/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export SOLID_APP_BACKEND_API=$APP_BACKEND_API
-npm run build
--- deployment/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-aws s3 sync ./build-artifact s3://$S3_BUCKET
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /*
